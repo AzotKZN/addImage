714ca2f8e4ebed3b49aaf6e9afe7cffeebf80a17
Azat Minvaliev <Miatozoff@gmail.com>
2015-02-13 20:51:02 +0200

    Initial Commit

---
+++ PhotoAdd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoAdd
+//
+//  Created by Азат on 13.02.15.
+//  Copyright (c) 2015 Azat Minvaliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
